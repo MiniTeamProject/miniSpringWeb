c81f8c904d2fd3dd2b38381e21b83c14e1c973a2
rwanda95do <dlsdnr2379@naver.com>
2024-10-16 17:33:44 +0900

    login success

--- src/main/java/user/dao/UserDAO.java
+++ src/main/java/user/dao/UserDAO.java
@@ -4,26 +4,15 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.ui.ModelMap;
 
 import user.bean.UserDTO;
 
 @Mapper
 public interface UserDAO {
 
-	public String userExistId(String id);
+	UserDTO userLogin(Map<String, String> map);
 	
-	public void userWrite(UserDTO userDTO);
-
-	public List<UserDTO> userList(Map<String, Integer> map);
 	
-	public int getTotalA();
-	
-	public int userUpdate(Map<String, Object> userMap);
-
-	public void userDelete(Map<String, String> deleteMap);
-
-    public List<UserDTO> selectUser(String id);
-    
-    
 
 }
